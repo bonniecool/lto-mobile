e777d662256db49fd7269c5d2d7e834273442e1d
Lee <al.s.apalat@gmail.com>
2016-10-14 02:24:52 +0800

    add images

--- site/src/Routes.js
+++ site/src/Routes.js
@@ -13,16 +13,22 @@ const history = syncHistoryWithStore(browserHistory, store);
 
 import Layout from './Layout';
 
+import Login from './pages/login';
+
 //PAGE
 import App from './App';
 import Search from './pages/search';
 import Scan from './pages/scan';
 
 export const Routes = () =>{
+
+	const hasLogin = sessionStorage.getItem('user') || false;
+
+
 	return(
 		<Provider store={ store }>
 			<Router history={ history } onUpdate={()=>{ window.scrollTo(0,0);}}>
-				<Route path="/" component={ Layout }>
+				<Route path="/" component={ (hasLogin) ? Layout : Login }>
 					<IndexRoute component={ App }/>
 					<Route path="/search" component={ Search }/>
 					<Route path="/scan" component={ Scan }/>
